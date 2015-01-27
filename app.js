db135ba08c9ca97a9a87b92df90e535898fa5b5d
bradbaris <brad@nomadspaceship.com>
2015-01-26 18:03:46 -1000

    whaaaat mod2

--- app.js
+++ app.js
@@ -2,41 +2,37 @@ function start(message){
   var player = {seal: 0};
   var computer = {seal: 0};
   console.log(message);
-  player.seal = prompt("what's yo weapon biotch?");
+  player.seal = prompt("What's yo weapon biotch?");
   computer.seal = Math.round(Math.random()* 2);
-  var fate = player + computer;
   this.compare(player, computer);
 }
 
-var compare = function(player, computer) {
-  if (outcome === 0 || outcome === 4) {
-    return this.start("renewal");
+function compare(player, computer) {
+  
+  var fate = ((player + computer)%2);
+
+  if ( fate === 0 ) {
+   
+    if ( player.seal === 0 ){
+      message = "Player Wins";
+    }
+    if ( computer.seal === 0 ){
+      message = "Computer Wins";
+    } 
+    if ( player.seal === 0 && computer.seal === 0 ) {
+      message = "TIE";
+    } 
+
   }
 
-  if (outcome === 1) {
-    if ( player.seal === 1 ) {
-      message = "Player Wins with PAPER";
-    } else {
-      message = "Computer Wins with PAPER";
-    }
-  } 
+  if ( fate === 1 ){
 
-  if (outcome === 2) {
-    if ( computer.seal === 0 ) {
-      message = "Computer Wins with ROCK";
-    }
-    if (computer.seal === 2) {
-      message = "Player Wins with ROCK";
-    }
-  } 
-
-  if (outcome === 3) {
-    if ( player.seal === 2 ) {
-      message = "Player Wins with SCISSORS";
+    if ( player.seal > computer.seal){
+      message = "Player Wins";
     } else {
-      message = "Computer Wins with SCISSORS";
+      message = "Computer Wins";
     }
-  } 
+  }
   
   return this.start(message);
 
