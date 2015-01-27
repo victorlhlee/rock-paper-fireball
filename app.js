dded046b76f69efe5ba71d14d9bc39a8fd6fb2de
bradbaris <brad@nomadspaceship.com>
2015-01-26 15:03:44 -1000

    initial borked commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function start(){
+  var player = {seal: 0};
+  var computer = {seal: 0};
+  player.seal = prompt("what's yo weapon biotch?");
+  computer.seal = Math.round(Math.random()* 2);
+  var fate = player + computer;
+  this.compare(player, computer);
+}
+
+var compare = function(player, computer) {
+  if (outcome === 0 || outcome === 2 || outcome === 4) {
+    return this.start();
+  }
+  if(outcome === 1) {
+    return player.seal; //whoever has the seal 1;
+  } 
+   computerChoice = "scissors cutting sh*t up!";
+};
+
+compare(choice1, choice2);
